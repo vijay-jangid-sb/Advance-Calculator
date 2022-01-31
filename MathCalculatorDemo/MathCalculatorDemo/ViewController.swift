9ba129d756d2f61b8d66086c60f0c344f6f8269c
vijay-jangid-sb <98740903+vijay-jangid-sb@users.noreply.github.com>
2022-01-31 16:14:52 +0530

    First time commited

---
+++ MathCalculatorDemo/MathCalculatorDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MathCalculatorDemo
+//
+//  Created by Vijay Jangid on 31/01/22.
+//
+
+import UIKit
+import MathCalculation_sdk
+ 
+
+class ViewController: UIViewController {
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
